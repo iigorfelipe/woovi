71b609d523ed065b4cdb4c93aef2c7f7e4275f00
Igor <iigorfelipe@gmail.com>
2024-07-17 01:08:31 -0300

    ⚡️ finalize TimeLine component

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,28 +1,20 @@
-export const Timeline = () => {
-  const events = [
-    {
-      id: 1,
-      content: {
-        um: '1ª entrada no Pix',
-        dois: 'R$ 15.300,00'
-      },
-      isActive: true,
-    },
-    {
-      id: 2,
-      content: {
-        um: '2ª no cartão',
-        dois: 'R$ 15.300,00'
-      },
-      isActive: false,
-    }
-  ];
+export type TimelineEvent = {
+  label: string;
+  value: string;
+  isCurrentStep: boolean;
+  isCompletedStep: boolean;
+};
 
+type TimeLineProps = {
+  events: TimelineEvent[];
+};
+
+export const Timeline = ({ events }: TimeLineProps) => {
   return (
     <div className="w-full flex flex-col gap-4">
       {
         events.map((event, index) => (
-          <div key={event.id} className="w-full flex items-center gap-2 relative">
+          <div key={event.label} className="w-full flex items-center gap-2 relative">
 
             {
               index !== events.length - 1 && (
@@ -30,16 +22,18 @@ export const Timeline = () => {
               )
             }
 
-            <div className={`w-4 h-4 border-2 rounded-full bg-white z-10 ${
-              event.isActive ? 'border-green-500' : 'border-gray-300'
-            }`} />
+            <div className={`w-4 h-4 border-2 rounded-full z-10 flex items-center justify-center ${
+              event.isCurrentStep ? 'border-green-500' : 'border-gray-300'
+            } ${event.isCompletedStep ? 'bg-green-500 border-green-500' : 'bg-white'}`}>
+              {event.isCompletedStep && (<img src="./checked.svg" className="w-[7px] h-1.5" />)}
+            </div>
 
             <time className="text-lg font-semibold leading-none text-[#4D4D4D]">
-              {event.content.um}
+              {event.label}
             </time>
 
             <span className="ml-auto text-lg font-extrabold">
-              {event.content.dois}
+              {event.value}
             </span>
 
           </div>
